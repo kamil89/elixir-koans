88ddbaa19af74cc20e68b5d583270d77fcddb3a5
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2018-01-03 19:33:15 +0100

    keyword lists solutions

--- lib/koans/07_keyword_lists.ex
+++ lib/koans/07_keyword_lists.ex
@@ -6,30 +6,30 @@ defmodule KeywordLists do
   koan "Like maps, keyword lists are key-value pairs" do
     kw_list = [foo: "bar"]
 
-    assert kw_list[:foo] == ___
+    assert kw_list[:foo] == "bar"
   end
 
   koan "Keys may be repeated, but only the first is accessed" do
     kw_list = [foo: "bar", foo: "baz"]
 
-    assert kw_list[:foo] == ___
+    assert kw_list[:foo] == "bar"
   end
 
   koan "You could access a second key by removing the first" do
     kw_list = [foo: "bar", foo: "baz"]
     [_|kw_list] = kw_list
 
-    assert kw_list[:foo] == ___
+    assert kw_list[:foo] == "baz"
   end
 
   koan "Keyword lists are just special syntax for lists of two-element tuples" do
-    assert [foo: "bar"] == [{___, ___}]
+    assert [foo: "bar"] == [{:foo, "bar"}]
   end
 
   koan "But unlike maps, the keys in keyword lists must be atoms" do
     not_kw_list = [{"foo", "bar"}]
 
-    assert_raise ArgumentError, fn -> not_kw_list[___] end
+    assert_raise ArgumentError, fn -> not_kw_list["foo"] end
   end
 
   koan "Conveniently keyword lists can be used for function options" do
@@ -41,7 +41,7 @@ defmodule KeywordLists do
       end
     end
 
-    assert transform.("good", upcase: true) == ___
-    assert transform.("good", upcase: false) == ___
+    assert transform.("good", upcase: true) == "GOOD"
+    assert transform.("good", upcase: false) == "good"
   end
 end
